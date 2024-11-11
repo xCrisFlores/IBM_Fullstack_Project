dcd4d41cfa221fce5e15239f6b75f7dfe9764998
xCrisFlores <cristian@gmail.com>
2024-11-11 11:37:40 -0500

    Backend services submission

--- database/app.js
+++ database/app.js
@@ -58,17 +58,32 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const dealers = await Dealerships.find();
+        res.json(dealers);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealers' });
+      }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    try {
+        const dealer = await Dealerships.find({state: req.params.state});
+        res.json(dealer);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealer' });
+      }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    try {
+        const dealer = await Dealerships.find({id: req.params.id});
+        res.json(dealer);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealer' });
+      }
 });
 
 //Express route to insert review
