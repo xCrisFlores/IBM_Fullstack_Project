09196b3735d26cb131f2d9f97d31fd1be1ddf050
xCrisFlores <cristian@gmail.com>
2024-11-11 12:13:04 -0500

    Car models submission

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -6,13 +6,9 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # path for registration
     path(route='register', view=views.registration, name='register'),
-    # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
-    # path for dealer reviews view
-
-    # path for add a review view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
