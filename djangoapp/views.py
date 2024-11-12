b959bb381b542862f421408a387c847385e9fac1
xCrisFlores <cristianxd235@gmail.com>
2024-11-11 20:26:37 -0500

    Dynamic pa ges submission

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -12,8 +12,8 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
-
+from .populate import initiate
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
